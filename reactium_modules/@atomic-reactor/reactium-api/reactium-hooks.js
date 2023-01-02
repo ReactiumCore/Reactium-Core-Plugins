201a388ac925a28c711ff250b4cdbaac4afb1c0e
Cam Tullos <cam@reactium.io>
2023-01-02 01:47:53 -1000

    Try to fix production build issues.

--- reactium_modules/@atomic-reactor/reactium-api/reactium-hooks.js
+++ reactium_modules/@atomic-reactor/reactium-api/reactium-hooks.js
@@ -2,9 +2,13 @@ import Reactium from 'reactium-core/sdk';
 
 Reactium.Hook.register(
     'sdk-init',
-    async SDK => {
-        const { default: API } = await import('./sdk');
-        Reactium.API = API;
+    async () => {
+        try {
+            const { default: API } = await import('./sdk');
+            Reactium.API = API;
+        } catch (err) {
+            console.log(err);
+        }
     },
     Reactium.Enums.highest,
     'REACTIUM-CORE-SDK-API',
