3e8d0bc67ff855e9318fe64bf68931612171667c
John Dillick <john@dillick.us>
2023-05-26 11:52:22 -0400

    The following SDK named exports are exported together:
    State - Same ReactiumSyncState event target as Reactium.State
    useUpdater() - Generic Render Trigger
    useAttachSyncState() - Used for attaching State 'change' event to rerender component
    useAttachHandle() - Used for attaching a registerd sync state handle to a component
    useDispatcher() - Used to get a dispatch function for State EventTarget
    useStateEffect() - Used to register event handlers for State EventTarget

--- reactium_modules/@atomic-reactor/reactium-core/sdk/named-exports/state.js
+++ reactium_modules/@atomic-reactor/reactium-core/sdk/named-exports/state.js
@@ -1,5 +1,12 @@
-import { Handle, ReactiumSyncState } from '@atomic-reactor/reactium-sdk-core';
+import {
+    Handle,
+    ReactiumSyncState,
+    ComponentEvent,
+} from '@atomic-reactor/reactium-sdk-core';
 import { useEffect, useState } from 'react';
+import cc from 'camelcase';
+import _ from 'underscore';
+import op from 'object-path';
 
 export const State = new ReactiumSyncState(window.state || {});
 
@@ -19,4 +26,37 @@ export const useAttachSyncState = (
 
 export const useAttachHandle = (name, updateEventName = 'change') => {
     return useAttachSyncState(Handle.get(`${name}.current`), updateEventName);
+};
+
+export const useDispatcher = ({ props = {}, state = State }) => (type, obj) => {
+    obj = _.isObject(obj) ? obj : {};
+
+    const evt = new ComponentEvent(type, obj);
+    const cb = op.get(props, cc(`on-${type}`));
+
+    if (_.isFunction(cb)) state.addEventListener(type, cb);
+    state.dispatchEvent(evt);
+    if (_.isFunction(cb)) state.removeEventListener(type, cb);
+};
+
+export const useStateEffect = (handlers = {}, deps) => {
+    const unsubs = [];
+
+    // sanitize handlers
+    Object.entries(handlers).forEach(([type, cb]) => {
+        if (
+            typeof cb === 'function' &&
+            !Object.values(op.get(State, ['listeners', type], {})).find(
+                f => f === cb,
+            )
+        ) {
+            unsubs.push(State.addEventListener(type, cb));
+        }
+    });
+
+    useEffect(() => {
+        return () => {
+            unsubs.forEach(unsub => unsub());
+        };
+    }, deps);
 };
