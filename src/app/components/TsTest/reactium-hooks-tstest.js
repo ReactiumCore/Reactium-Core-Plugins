cf40a2fbea15fac697b7ddb1265d7d112aab9cd1
John Dillick <john@dillick.us>
2024-05-03 10:06:19 -0400

    Remove plugable from deps.

--- src/app/components/TsTest/reactium-hooks-tstest.js
+++ src/app/components/TsTest/reactium-hooks-tstest.js
@@ -1,13 +1,20 @@
-// /**
-//  * -----------------------------------------------------------------------------
-//  * Reactium Plugin TsTest
-//  * -----------------------------------------------------------------------------
-//  */
-// (async () => {
-//     const { Hook, Enums, Component } = await import('@atomic-reactor/reactium-core/sdk');
+/**
+ * -----------------------------------------------------------------------------
+ * Reactium Plugin TsTest
+ * -----------------------------------------------------------------------------
+ */
+(async () => {
+    const { Hook, Enums, Component } = await import(
+        '@atomic-reactor/reactium-core/sdk'
+    );
 
-//     Hook.register('plugin-init', async () => {
-//         const { TsTest } = await import('./TsTest');
-//         Component.register('TsTest', TsTest);
-//     }, Enums.priority.normal, 'plugin-init-TsTest');
-// })();
+    Hook.register(
+        'plugin-init',
+        async () => {
+            const { TsTest } = await import('./TsTest.tsx');
+            Component.register('TsTest', TsTest);
+        },
+        Enums.priority.normal,
+        'plugin-init-TsTest',
+    );
+})();
